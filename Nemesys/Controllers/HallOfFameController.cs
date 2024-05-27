4702dc1e64924c2b0f525f81ac81c980d3d7da3d
Noemi Garcia <noemi.garcia.23@um.edu.mt>
2024-05-27 18:56:13 +0200

    Fix errors

--- Nemesys/Controllers/HallOfFameController.cs
+++ Nemesys/Controllers/HallOfFameController.cs
@@ -1,34 +1,50 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Nemesys.Models;
+using Nemesys.ViewModels;
 
 namespace Nemesys.Controllers
 {
-    public class HallOfFameController : Controller{
-        
+    public class HallOfFameController : Controller
+    {
+
         private readonly IReportsRepository _reportsRepository;
         private readonly UserManager<User> _userManager;
+        private readonly ILogger<HallOfFameController> _logger;
 
-        public HallOfFameController(IReportsRepository reportsRepository, UserManager<User userManager){
+
+        public HallOfFameController(IReportsRepository reportsRepository, UserManager<User> userManager, ILogger<HallOfFameController> logger)
+        {
 
             _reportsRepository = reportsRepository;
             _userManager = userManager;
+            _logger = logger;
 
         }
 
-        public async Task<IActionResult> Index(){
+        public async Task<IActionResult> Index()
+        {
+            try {
+                var currentYear = DateTime.Now.Year;
+                var reports = _reportsRepository.GetAllReports().Where(r => r.DateOfReport.Year == currentYear).ToList();
 
-            var currentYear = DateTime.Now.Year;
-            var reports = _reportsRepository.GetAllReports().Where(r => r.DateOfReport.Year == currentYear).ToList();
+                var userReportStats = reports.GroupBy(r => r.UserId).Select(g => new UserHallOfFameViewModel
+                {
+                    UserName = _userManager.FindByIdAsync(g.Key).Result.UserName,
+                    TotalReports = g.Count(),
+                    TotalUpvotes = g.Sum(r => r.Upvotes),
+                    getRatioUpvotesReport = Math.Round(g.Sum(r => r.Upvotes) / (double)g.Count(), 2),
+                }).OrderByDescending(u => u.getRatioUpvotesReport).ToList();
+                return View(userReportStats);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return View("Error");
+            }
 
-            var userReportStats = reports.GroupBy(r =>r.UserID).Select(g => new UserHallOfFameViewModel{
-                UserName = _userManager.FindByIdAsync(g.key).Result.Username;
-                TotalReports = g.count();
-                TotalUpvotes = g.Sum(r =>r.upvotes);
-                getRatioUpvotesReport = g.Sum(r =>r.upvotes) / (double)g.count();
-            })
-            OrderByDescending(u => u.getRatioUpvotesReport).ToList();
-
-            return View(userReportStats);
-            
         }
 
     }
+}
