d2f2c87c3481652eb066c7c30dd1138c7c521065
calvam10 <91256859+calvam10@users.noreply.github.com>
2024-05-27 16:58:27 +0200

    UserHallOfFame View Model created

---
+++ Nemesys/ViewModels/UserHallOfFameViewModel.cs
@@ -0,0 +1,12 @@
+namespace Nemesys.ViewModels
+{
+    public class UserHallOfFameViewModel{
+
+        public string UserName { get; set; }
+        public int TotalUpvotes { get; set; }
+        public int TotalReports { get; set; }
+        public double getRatioUpvotesReport { get; set; }
+
+    }
+}
+
